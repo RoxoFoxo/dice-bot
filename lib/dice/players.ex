2017b7aafb01d8b0944b39a2b84a3972be761c67
RoxoFoxo <gab.apfprado@gmail.com>
2022-06-01 17:46:22 -0300

    Feat: Add users table

---
+++ lib/dice/players.ex
@@ -0,0 +1,48 @@
+defmodule Dice.Players do
+  @moduledoc """
+  This context is used to add and update user info.
+  """
+  import Ecto.Query, warn: false
+
+  alias Dice.Players.User
+  alias Dice.Repo
+
+  @doc """
+  %{
+    "first_name" => "Roxo",
+    "id" => 1001251536,
+    "is_bot" => false,
+    "language_code" => "en",
+    "last_name" => "💜",
+    "username" => "RoxoFoxo"
+  }
+  """
+  def check_user(user) do
+    stored_user = Repo.get_by(User, telegram_id: user["id"])
+    attrs = %{username: user["username"], name: user["first_name"], telegram_id: user["id"]}
+
+    with %User{} <- stored_user,
+         true <- stored_user.name == user["first_name"],
+         true <- stored_user.username == user["username"] do
+      :ok
+    else
+      nil ->
+        add_user(attrs)
+
+      false ->
+        update_user(stored_user, attrs)
+    end
+  end
+
+  defp add_user(attrs) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  defp update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+end
